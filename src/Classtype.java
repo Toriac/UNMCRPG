3a67a68aed43a65902e5d034f83f400a6f5df82e
Phillip Halpin <p.d.halpin@gmail.com>
2012-02-07 00:51:43 -0500

    Update with Constructor & variable name change.

--- src/Classtype.java
+++ src/Classtype.java
@@ -2,16 +2,20 @@
 public class Classtype {
 	
 	/* Vars */
-	private String _name = new String();
+	private String name = new String();
 	
+	/* Constructor */
+	Classtype(){
+		this.name = "";
+	}
 	
 	/* Methods */
 	public String getName(){
-		return this._name;
+		return this.name;
 	}
 	
 	public void setName(String incName){
-		this._name = incName;
+		this.name = incName;
 	}
 	
 }
