79c0adda0517fdd0e83d7f76a1ebb41bd6ee168d
Phillip Halpin <p.d.halpin@gmail.com>
2012-02-07 00:39:50 -0500

    Base project.

---
+++ src/Classtype.java
@@ -0,0 +1,17 @@
+
+public class Classtype {
+	
+	/* Vars */
+	private String _name = new String();
+	
+	
+	/* Methods */
+	public String getName(){
+		return this._name;
+	}
+	
+	public void setName(String incName){
+		this._name = incName;
+	}
+	
+}
