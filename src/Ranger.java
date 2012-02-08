99e2884b1ad822814515e53dda14a50d8e1f40bf
Michael Sara <ToriacDarkmoon@gmail.com>
2012-02-07 18:07:14 -0600

    Added in shells for the classes.

---
+++ src/Ranger.java
@@ -0,0 +1,39 @@
+
+ // TODO: Everything. Namely, stats being imported and exported from save to load.
+
+
+public class Ranger {
+	
+	// Ye Olde Redneck. 
+	
+	public void levelUp() {
+		
+		
+		
+		// Arrows don't kill people, rangers don't either. 
+	
+	}
+	
+	
+	void save() {
+		
+		
+		
+	
+		// I'm surprised you found the button to save.
+		
+		
+	}
+	
+	
+	void load() {
+		
+	 
+		
+		// PETA wants to pay us $10,000 to remove this button. We're considering it.
+		
+       
+		
+	}	
+
+}
---
+++ src/Warrior.java
@@ -0,0 +1,39 @@
+
+ // TODO: Everything. Namely, stats being imported and exported from save to load.
+
+public class Warrior {
+      
+	// It's like playing a class that takes skill and then removing the skill part.
+	
+	public void levelUp() {
+		
+		
+		
+		// You know, you do some shit, you get some xp and then ASJDJASFJ WTF I'M POWERFUL
+	
+	}
+	
+	
+	void save() {
+		
+		
+		
+	
+		// Not even death can save... Wait, yeah, we have an app for this.
+		
+		
+	}
+	
+	
+	void load() {
+		
+	 
+		
+		// Like a freighter full of awesome, here is your previous stat count. 
+		
+       
+		
+	}	
+	
+	
+}
